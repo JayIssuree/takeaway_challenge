d67be07136e15becab4ad12f0259535ecf7a9885
JayIssuree <jay_issuree@hotmail.co.uk>
2021-02-09 17:50:10 +0000

    added the menu_printer to the menu class

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,11 +1,17 @@
-require 'dish'
+require_relative 'menu_printer'
+require_relative 'dish'
 
 class Menu
 
-    attr_reader :dishes
+    attr_reader :dishes, :printer
 
-    def initialize(dishes:)
+    def initialize(dishes:, printer: MenuPrinter.new)
         @dishes = dishes
+        @printer = printer
+    end
+
+    def print_menu
+        printer.print_dishes(dishes)
     end
     
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,12 +6,22 @@ describe Menu do
     let(:dish_b) { double :dish, :name => "Burrito", :price => 8 }
     let(:dish_c) { double :dish, :name => "Burger", :price => 5 }
     let(:array_of_dishes) { [dish_a, dish_b, dish_c] }
-    let(:subject) { described_class.new(dishes: array_of_dishes) }
+    let(:menu_printer) { double :menu_printer }
+    let(:subject) { described_class.new(dishes: array_of_dishes, printer: menu_printer) }
     
     describe '#initialization' do
         
         it 'is initialized with an array of dishes' do
             expect(subject.dishes).to eq(array_of_dishes)
+        end
+
+    end
+
+    describe '#print_menu(dishes)' do
+        
+        it 'calls menu_printer.print_dishes(dishes)' do
+            expect(menu_printer).to receive(:print_dishes).with(array_of_dishes)
+            subject.print_menu
         end
 
     end
